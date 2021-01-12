e8d226321a84cf86772dc74be6fa23bcaf45aca7
ikeda-shoki <ikeda963552@gmail.com>
2021-01-12 14:55:41 +0000

    [Finish]5-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!
+  before_action :ensure_correct_book, only: [:edit, :update]
 
   def create
     @user = current_user
@@ -41,13 +42,22 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    @book = Book.find(params[:id])
-    @book.destroy
+    @books = Book.find(params[:id])
+    @user = @books.user
+    @books.destroy
     redirect_to books_path
   end
 
   private
-  def book_params
-    params.require(:book).permit(:title, :body)
-  end
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
+  
+    def ensure_correct_book
+      @book = Book.find(params[:id])
+      @user = @book.user
+      if @user != current_user
+        redirect_to books_path
+      end
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update]
 
   def show
     @book = Book.new
@@ -30,5 +31,12 @@ class UsersController < ApplicationController
   private
     def user_params
       params.require(:user).permit(:name, :introduction, :profile_image)
+    end
+    
+    def ensure_correct_user
+      @user = User.find(params[:id])
+      if @user != current_user
+        redirect_to user_path(current_user)
+      end
     end
 end
